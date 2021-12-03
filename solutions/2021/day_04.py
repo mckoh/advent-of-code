7f236c1748e878736be7dc224d6b79b1b72fe9fe
mckoh <michael.kohlegger@gmail.com>
2021-12-03 21:55:14 +0100

    prepared day 04 puzzle solution

---
+++ solutions/2021/day_04.py
@@ -0,0 +1,34 @@
+"""
+Day 04 Solution for Advent of Code
+Date: 2021-12-04
+Author: Michael Kohlegger
+"""
+
+# %%
+from aocd import submit
+from aocd import get_data
+from util_ import to_int, to_complex, to_str
+
+input_data = to_str(get_data())
+
+test_data = []
+
+
+# %%
+def solution_1(data):
+    pass
+
+
+def solution_2(data):
+    pass
+
+
+# %%
+submit(
+    solution_1(data=input_data),
+)
+
+# %%
+submit(
+    solution_2(data=input_data),
+)
