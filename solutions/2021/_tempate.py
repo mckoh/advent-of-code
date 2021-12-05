56343a91c72e4cca3ceb349c9354d348923d2ec4
mckoh <michael.kohlegger@gmail.com>
2021-12-05 13:48:15 +0100

    changed utilities import

--- solutions/2021/_tempate.py
+++ solutions/2021/_tempate.py
@@ -7,8 +7,7 @@ Author: Michael Kohlegger
 # %%
 from aocd import submit
 from aocd import get_data
-
-from util_ import to_int, to_complex, to_str
+from _utilities import to_int, to_complex, to_str
 
 
 input_data = to_str(get_data())
