356be9a59f591c004959060428e576918c636238
Michael Kohlegger MLM <michael.kohlegger@fh-kufstein.ac.at>
2021-12-22 17:46:02 +0100

    prepared day 20

---
+++ solutions/2021/day_20.py
@@ -0,0 +1,34 @@
+"""
+Day xxx Solution for Advent of Code
+Date: xxx
+Author: Michael Kohlegger
+"""
+
+# %%
+from aocd import submit
+from aocd import get_data
+from _utilities import to_int, to_complex, to_str
+
+
+input_data = to_str(get_data())
+test_data = []
+
+
+# %%
+def solution_1(data):
+    pass
+
+
+def solution_2(data):
+    pass
+
+
+# %%
+submit(
+    solution_1(data=input_data),
+)
+
+# %%
+submit(
+    solution_2(data=input_data),
+)
